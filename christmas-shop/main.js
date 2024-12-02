1c2b252fca3700a7731689e3c3ea26abe057c787
Nikita <nbystritsky@gmail.com>
2024-12-03 01:06:06 +0500

    fix: update url arrow

---
+++ christmas-shop/main.js
@@ -0,0 +1,43 @@
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    var clock = document.querySelector('class');
+    var daysSpan = document.querySelector('.day');
+    var hoursSpan = document.querySelector('.hour');
+    var minutesSpan = document.querySelector('.minutes');
+    var secondsSpan = document.querySelector('.seconds');
+  
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = (t.hours);
+      minutesSpan.innerHTML = (t.minutes);
+      secondsSpan.innerHTML = (t.seconds);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  var deadline="January 01 2025 00:00:00 UTC+0";
+  initializeClock('timer', deadline);
+
+  
