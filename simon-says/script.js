f0422fef0782e13e5c2b84937f2a45f6ebba8c25
Nikita <nbystritsky@gmail.com>
2025-01-09 20:45:13 +0500

    feat: add game container

---
+++ simon-says/script.js
@@ -0,0 +1,8 @@
+const body = document.body;
+
+//game container
+const mainContainer = document.createElement('div');
+mainContainer.classList.add('main-container');
+body.appendChild(mainContainer);
+
+//level buttons
