6af8c9c2af3e15e775b02082e5ca5108ff242c2f
Nikita <nbystritsky@gmail.com>
2025-01-10 13:36:52 +0500

    feat: add levels button and new game button

--- simon-says/script.js
+++ simon-says/script.js
@@ -5,4 +5,22 @@ const mainContainer = document.createElement('div');
 mainContainer.classList.add('main-container');
 body.appendChild(mainContainer);
 
+//level container
+const levelContainer = document.createElement('div');
+levelContainer.classList.add('level-container');
+mainContainer.appendChild(levelContainer);
+
 //level buttons
+const levels = ['easy', 'medium', 'hard'];
+levels.forEach(level => {
+    const button = document.createElement('button');
+    button.textContent = level;
+    button.onclick = () => startGame(level);
+  levelContainer.appendChild(button);
+});
+
+//new game button
+const newGameButton = document.createElement('button');
+newGameButton.classList.add('new-game-btn');
+newGameButton.textContent = 'new game';
+mainContainer.appendChild(newGameButton);
